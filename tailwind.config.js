6bf6d77e50bac1f314d3afacd098b28659f0293e
Leandro Bataglia <leandro.bataglia@gmail.com>
2022-08-02 15:09:20 -0300

    added navbar in users page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./**/*.{html,js}", ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+  
+}
